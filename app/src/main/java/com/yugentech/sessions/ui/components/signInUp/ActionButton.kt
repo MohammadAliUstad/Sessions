abbbfcf66c26480fc28cfb7ff65d4d6279867a96
Mohammad Ali <mohammadaliustad@gmail.com>
2025-09-10 16:42:10 +0530

    Notification problems

--- app/src/main/java/com/yugentech/sessions/ui/components/signInUp/ActionButton.kt
+++ app/src/main/java/com/yugentech/sessions/ui/components/signInUp/ActionButton.kt
@@ -1,4 +1,4 @@
-package com.yugentech.sessions.ui.components
+package com.yugentech.sessions.ui.components.signInUp
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.tween
