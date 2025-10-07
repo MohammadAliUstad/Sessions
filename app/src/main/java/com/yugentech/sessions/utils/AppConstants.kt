98a71a5371bfa8d842238cb5da1c4a153e2bac2d
Mohammad Ali <mohammadaliustad@gmail.com>
2025-10-07 15:45:00 +0530

    Minor Improvements

--- app/src/main/java/com/yugentech/sessions/utils/AppConstants.kt
+++ app/src/main/java/com/yugentech/sessions/utils/AppConstants.kt
@@ -11,7 +11,7 @@ object AppConstants {
     const val TERMS_OF_SERVICE_URL = "https://sites.google.com/view/sessionstermsofservice/home"
     const val KOFI_URL = "https://ko-fi.com/yugentech"
     const val SUPPORT_EMAIL = "mailto:yugentech.kazuki@gmail.com"
-    const val FEEDBACK_SUBJECT = "App Feedback: Sessions"
+    const val PABLO_STANLEY_URL = "https://www.pablostanley.com/"
     const val PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=$APP_PACKAGE_NAME"
     const val SHARE_MESSAGE = "Check out Sessions, a minimal pomodoro focus app\n$PLAY_STORE_URL"
     const val MARKET_URL = "market://details?id=com.yugentech.sessions"
