3ef1704262f81059e5709b393e173bc38d315a85
Mohammad Ali <mohammadaliustad@gmail.com>
2025-10-10 15:45:00 +0530

    Minor Improvements

--- app/src/main/java/com/yugentech/sessions/auth/state/AuthState.kt
+++ app/src/main/java/com/yugentech/sessions/auth/state/AuthState.kt
@@ -1,14 +1,15 @@
-package com.yugentech.sessions.authentication.state
+package com.yugentech.sessions.auth.state
 
 import android.app.PendingIntent
-import com.yugentech.sessions.models.UserData
+import com.yugentech.sessions.user.model.UserData
 
 // Data class holding the current state of the authentication UI
 data class AuthState(
+    val isInitializing: Boolean = true,
     val isLoading: Boolean = false,
     val userId: String? = null,
     val error: String? = null,
     val intent: PendingIntent? = null,
     val userData: UserData? = null,
-    val isUserLoggedIn: Boolean = true
+    val isUserLoggedIn: Boolean = false
 )
