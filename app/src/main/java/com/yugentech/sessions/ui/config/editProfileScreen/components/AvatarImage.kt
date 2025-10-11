cb82f714a8fa7231af44bcfbd7f65ca4496b15eb
Mohammad Ali <mohammadaliustad@gmail.com>
2025-10-11 15:45:00 +0530

    Minor Improvements

--- app/src/main/java/com/yugentech/sessions/ui/config/editProfileScreen/components/AvatarImage.kt
+++ app/src/main/java/com/yugentech/sessions/ui/config/editProfileScreen/components/AvatarImage.kt
@@ -1,4 +1,4 @@
-package com.yugentech.sessions.ui.config.components.editProfileScreen
+package com.yugentech.sessions.ui.config.editProfileScreen.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
