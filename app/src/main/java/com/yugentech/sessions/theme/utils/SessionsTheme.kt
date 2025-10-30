0042870e2c1ae367ead23d69eb0408532f8a302f
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2025-10-30 11:59:30 +0530

    Further fixes

--- app/src/main/java/com/yugentech/sessions/theme/utils/SessionsTheme.kt
+++ app/src/main/java/com/yugentech/sessions/theme/utils/SessionsTheme.kt
@@ -2,6 +2,7 @@ package com.yugentech.sessions.theme.utils
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.yugentech.sessions.ui.ProvideDesignTokens
 
 @Composable
 fun SessionsTheme(
@@ -10,9 +11,11 @@ fun SessionsTheme(
 ) {
     val colorScheme = getColorScheme(themeConfiguration = themeConfiguration)
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = AppTypography,
-        content = content
-    )
+    ProvideDesignTokens {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = AppTypography,
+            content = content
+        )
+    }
 }
