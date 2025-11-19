e3ca1f770d911163b61f2da17bf471e7a808159f
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2025-11-19 14:53:09 +0530

    Code Refactoring

---
+++ app/src/main/java/com/yugentech/sessions/ui/dash/components/homeScreen/ActionButtons.kt
@@ -0,0 +1,67 @@
+package com.yugentech.sessions.ui.dash.components.homeScreen
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.yugentech.sessions.theme.tokens.components
+import com.yugentech.sessions.theme.tokens.elevation
+import com.yugentech.sessions.theme.tokens.icons
+import com.yugentech.sessions.theme.tokens.spacing
+
+@Composable
+fun ActionButton(
+    isStudying: Boolean,
+    onPlayPause: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    // Animated container color
+    val containerColor by animateColorAsState(
+        targetValue = if (isStudying)
+            MaterialTheme.colorScheme.tertiaryContainer
+        else
+            MaterialTheme.colorScheme.primary,
+        animationSpec = tween(durationMillis = 400),
+        label = "button-container-color"
+    )
+
+    // Animated content color
+    val contentColor by animateColorAsState(
+        targetValue = if (isStudying)
+            MaterialTheme.colorScheme.onTertiaryContainer
+        else
+            MaterialTheme.colorScheme.onPrimary,
+        animationSpec = tween(durationMillis = 400),
+        label = "button-content-color"
+    )
+
+    FloatingActionButton(
+        onClick = onPlayPause,
+        modifier = modifier.
+        size(MaterialTheme.components.fabSize)
+            .padding(MaterialTheme.spacing.xsSmall),
+        shape = CircleShape,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        elevation = FloatingActionButtonDefaults.elevation(
+            defaultElevation = MaterialTheme.elevation.level0
+        )
+    ) {
+        Icon(
+            imageVector = if (isStudying) Icons.Default.Pause else Icons.Default.PlayArrow,
+            contentDescription = if (isStudying) "Pause" else "Start",
+            modifier = Modifier.size(MaterialTheme.icons.mediumLarge)
+        )
+    }
+}
