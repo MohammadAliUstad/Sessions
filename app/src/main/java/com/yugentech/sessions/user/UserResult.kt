bfd17c174a94e018f2c36ce9323df0af01997921
Mohammad Ali <mohammadaliustad@gmail.com>
2025-12-23 16:45:00 +0530

    Improve code structure

--- app/src/main/java/com/yugentech/sessions/user/UserResult.kt
+++ app/src/main/java/com/yugentech/sessions/user/UserResult.kt
@@ -1,6 +1,5 @@
 package com.yugentech.sessions.user
 
-// Generic wrapper to handle data Success, Error, and Loading states
 sealed class UserResult<out T> {
     object Loading : UserResult<Nothing>()
     data class Success<T>(val data: T) : UserResult<T>()
