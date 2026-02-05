8e221bf508759bc698e1d74340a772e2dcba0759
Mohammad Ali <mohammadaliustad@gmail.com>
2026-02-05 15:45:00 +0530

    Fixing of Session pipeline

---
+++ app/src/main/java/com/yugentech/sessions/notification/model/NotificationType.kt
@@ -0,0 +1,7 @@
+package com.yugentech.sessions.notification.model
+
+// Distinguishes between active timer updates and scheduled future reminders
+enum class NotificationType {
+    ACTIVE,
+    SCHEDULED
+}
