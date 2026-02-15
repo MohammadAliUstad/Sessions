b8581143ed7b7c521347bd195419d981933ec3ba
Mohammad Ali <mohammadaliustad@gmail.com>
2026-02-15 15:45:00 +0530

    Clean-Up

--- app/src/main/java/com/yugentech/sessions/user/result/UserResult.kt
+++ app/src/main/java/com/yugentech/sessions/user/result/UserResult.kt
@@ -1,2 +1,7 @@
-package com.yugentech.sessions.user.result 
+package com.yugentech.sessions.user.result
 
+sealed class UserResult<out T> {
+    object Loading : UserResult<Nothing>()
+    data class Success<T>(val data: T) : UserResult<T>()
+    data class Error(val message: String, val exception: Throwable? = null) : UserResult<Nothing>()
+}
