ffc7c0fcd6f84ffc47d0b576710480b57c63d02f
Mohammad Ali Ustad <mohammadaliustad@gmail.com>
2026-09-09 23:27:19 +0530

    Bug fixes

--- app/src/main/java/com/yugentech/sessions/theme/datastore/ThemeDataStore.kt
+++ app/src/main/java/com/yugentech/sessions/theme/datastore/ThemeDataStore.kt
@@ -33,9 +33,20 @@ class ThemeDataStore(
                 AppFont.Google
             }
 
+            val themeMode = try {
+                ThemeMode.valueOf(prefs[THEME_MODE_KEY] ?: ThemeMode.SYSTEM.name)
+            } catch (e: IllegalArgumentException) {
+                ThemeMode.SYSTEM
+            }
+            val colorTheme = try {
+                ColorTheme.valueOf(prefs[COLOR_THEME_KEY] ?: ColorTheme.SESSIONS.name)
+            } catch (e: IllegalArgumentException) {
+                ColorTheme.SESSIONS
+            }
+
             ThemeConfiguration(
-                themeMode = ThemeMode.valueOf(prefs[THEME_MODE_KEY] ?: ThemeMode.SYSTEM.name),
-                colorTheme = ColorTheme.valueOf(prefs[COLOR_THEME_KEY] ?: ColorTheme.SESSIONS.name),
+                themeMode = themeMode,
+                colorTheme = colorTheme,
                 useDynamicColors = prefs[USE_DYNAMIC_COLORS_KEY] ?: false,
                 isAmoledMode = prefs[IS_AMOLED_MODE_KEY] ?: false,
                 appFont = appFont
